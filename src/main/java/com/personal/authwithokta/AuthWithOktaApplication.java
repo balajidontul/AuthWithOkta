5d9ea1f440840fce8a71f2da23da4270bd0cdaf4
unknown <balajidontulofficial@outlook.com>
2024-05-08 11:47:12 +0530

    Initial commit

---
+++ src/main/java/com/personal/authwithokta/AuthWithOktaApplication.java
@@ -0,0 +1,13 @@
+package com.personal.authwithokta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWithOktaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthWithOktaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/authwithokta/AuthWithOktaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.authwithokta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthWithOktaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
